2bc8aa205944232ae98ad84e52937fb580f00c9d
arunladha <arunladha@rancelab.com>
2017-09-01 17:36:31 +0530

    minor changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,3 @@
-// let settings = {
-//     baseUrl: 'http://61.16.131.163:3000',
-//     urlMaps: {
-//         'http:get:test': 'test'
-//     }
-// };
-// export {settings};
 let envs = {
     staging: 'http://223.29.199.212:3002',
     qa: 'http://61.16.131.166:3002',
