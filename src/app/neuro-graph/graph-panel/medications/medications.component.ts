045ea8cdab90728f8d962cdf32f96f5d306cd340
Sudipto Mukherjee <sudiptokush@gmail.com>
2017-09-06 18:13:24 +0530

    medication classification

--- src/app/neuro-graph/graph-panel/medications/medications.component.ts
+++ src/app/neuro-graph/graph-panel/medications/medications.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BrokerService } from '../../../fire-base/broker.service';
-import { allMessages, allHttpMessages } from '../../neuro-graph.config';
+import { allMessages, allHttpMessages, medication } from '../../neuro-graph.config';
 import * as d3 from 'd3';
 
 @Component({
@@ -10,6 +10,12 @@ import * as d3 from 'd3';
 })
 export class MedicationsComponent implements OnInit {
   subscriptions: any;
+
+  allMedications: any;
+  dmt: Array<any> = [];
+  vitaminD: Array<any> = [];
+  otherMeds: Array<any> = [];
+
   constructor(private brokerService: BrokerService) { }
 
   ngOnInit() {
@@ -24,7 +30,8 @@ export class MedicationsComponent implements OnInit {
       .brokerService
       .filterOn(allHttpMessages.httpGetMedications)
       .subscribe(d => {
-        console.log(d.data);
+        this.allMedications = d.data;
+        this.categorizeMedication();
       }));
     this.drawMedications();
   }
@@ -57,7 +64,54 @@ export class MedicationsComponent implements OnInit {
       .attr("cy", 50)
       .attr('fill', 'red');
   }
+  categorizeMedication() {
+    this.allMedications.EPIC.patients[0].medicationOrders.map((m) => {
 
+      //checking for DMT
+      medication.DMT.genericName.map((d) => {
+        if (m.medication.simple_generic_name.toUpperCase() == d.toUpperCase()) {
+          this.dmt.push(m);
+        }
+      });
+
+      //checking for Vitamin D
+      medication.VitaminD.id.map((v) => {
+        if (m.medication.id == v) {
+          this.vitaminD.push(m);
+        }
+      });
+
+      //checking for Other Meds
+      medication.OtherMeds.id.map((o) => {
+        if (m.medication.id == o) {
+          this.otherMeds.push(m);
+        }
+      });
+
+      //checking for Other Meds
+      medication.OtherMeds.associatedDiagnosesId.map((o) => {
+        // check if associatedDiagnoses is empty
+        if (m.associatedDiagnoses.length != 0) {
+          m.associatedDiagnoses.map((ad)=>{
+            // check if associatedDiagnoses has a key named code_sets
+            if (ad.hasOwnProperty("code_sets")) {
+                // check if code_sets have any value
+                if (ad.code_sets.length != 0) {
+                  ad.code_sets.map((mc) => {
+                    if (mc.mapped_code == o) {
+                      //check if this medicationOrder json object is already pushed
+                      if(this.otherMeds.indexOf(m)==-1)
+                      this.otherMeds.push(m);
+                    }
+                  });
+                }
+              }
+          });
+        }
+      });
+    });
+    alert("DMT - " + this.dmt.length + " / Vitamin D - " + this.vitaminD.length + " / Other Meds - " + this.otherMeds.length);
+  }
 
   ngOnDestroy() {
     this
