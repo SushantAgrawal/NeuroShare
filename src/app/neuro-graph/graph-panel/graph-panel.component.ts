4e6023b06a04ca4dfb7e37486a63673fde62fa91
Sudipto Mukherjee <sudiptokush@gmail.com>
2017-09-08 19:17:29 +0530

    Virtual Case Load design

--- src/app/neuro-graph/graph-panel/graph-panel.component.ts
+++ src/app/neuro-graph/graph-panel/graph-panel.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({ selector: 'app-graph-panel', templateUrl: './graph-panel.component.html', styleUrls: ['./graph-panel.component.sass'] })
 export class GraphPanelComponent implements OnInit {  
+  isEdssSelected: boolean= false;
   constructor() { }
   ngOnInit() {    
   }
